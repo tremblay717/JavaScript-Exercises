652a00a1db4846d5552c43fb1d0ef764bd65ada2
christophe <christo_hurley@hotmail.com>
2022-06-23 08:26:31 -0400

    fixed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    let value = "";
+    if (num < 0) {
+      return "ERROR";
+    }
+    else {
+      let i = 1;
+      while (i <= num) {
+  
+        value += string;
+        i++;
+      }
+      return value;
+    }
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
