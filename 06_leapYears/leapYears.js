377e3ee6fc5d0fbc9cb07825f9a6b19cc3d878c6
christophe <christo_hurley@hotmail.com>
2022-06-24 19:49:24 -0400

    finished leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
-};
+    if ((year % 100 == 0 && (year % 400 != 0)) || (year % 4 !== 0)) {
+        return false;
+    } else if ((year % 4 === 0) || (year % 400 == 0)) {
+        return true
+    }
+
+
+}
 
 // Do not edit below this line
 module.exports = leapYears;
