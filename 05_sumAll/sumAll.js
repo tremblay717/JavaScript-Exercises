0fee56b8f8b162a417bcee5942838d8a8903cd94
christophe <christo_hurley@hotmail.com>
2022-06-24 17:46:01 -0400

    fixed semicolon

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function (p1, p2) {
 
     if
         (((p1 < 0 || p2 < 0)) || (typeof (p1) !== 'number' || typeof (p2) !== 'number')) {
-        return 'ERROR'
+        return 'ERROR';
     } else {
         let list = [p1, p2];
         let sort = list.sort((a, b) => a - b);
