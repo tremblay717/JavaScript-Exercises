36008fcf0d96776aa9c90e38ffd9000f460792e9
christophe <christo_hurley@hotmail.com>
2022-06-25 09:17:23 -0400

    fixed format

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,19 @@
 const reverseString = function (word) {
 
     if (word == "" || word.length == 1) {
-        return word
+        return word;
+
     } else if (word.length >= 2) {
-        let reverse = ""
+
+        let reverse = "";
         let i = word.length;
+
         while (i >= 0) {
+
             reverse += word.slice(i - 1, i);
             i--;
         }
+
         return reverse;
     }
 };
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,7 @@ const ftoc = function (fTemp) {
 
   const cTemp = ((fTemp - 32) * (5 / 9));
 
-  return Math.round(cTemp * 10) / 10
+  return Math.round(cTemp * 10) / 10;
 
 };
 
@@ -10,7 +10,7 @@ const ctof = function (cTemp) {
 
   const fTemp = (cTemp * 1.8) + 32;
 
-  return Math.round(fTemp * 10) / 10
+  return Math.round(fTemp * 10) / 10;
 };
 
 // Do not edit below this line
