c43fc1d2977942f2891b3b249d681318242ca364
christophe <christo_hurley@hotmail.com>
2022-06-25 07:04:13 -0400

    finished ex 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function (fTemp) {
+
+  const cTemp = ((fTemp - 32) * (5 / 9));
+
+  return Math.round(cTemp * 10) / 10
 
 };
 
-const ctof = function() {
+const ctof = function (cTemp) {
 
+  const fTemp = (cTemp * 1.8) + 32;
+
+  return Math.round(fTemp * 10) / 10
 };
 
 // Do not edit below this line
