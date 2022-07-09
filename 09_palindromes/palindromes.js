07d04e7114ec62f17073551c6e29c047038a254c
christophe <christo_hurley@hotmail.com>
2022-07-09 11:00:55 -0400

    Completed palindromes Exercice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+
+    let pattern = /[" ".,?!]/g;
+    let test = word.replace(pattern, "").toLowerCase();
+    let newArray = []
+
+    for (i = 1; i < test.length; i++) {
+
+        if (test[i - 1] == test[test.length - i]) {
+            newArray.push(1);
+        } else(newArray.push(0))
+    }
+
+    if (newArray.includes(0)) {
+        return false;
+    } else {
+        return true
+    }
 
 };
 
