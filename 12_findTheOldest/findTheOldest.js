f5ec7292f8b6b1a1899aa9c62c89b5fc005f2481
christophe <christo_hurley@hotmail.com>
2022-07-09 13:26:36 -0400

    finished findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+
+    for (let i = 0; i < people.length; i++) {
+
+        if (Object.keys(people[i]).indexOf('yearOfDeath') == 2) {
+            people[i]['age'] = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            let year = new Date().getFullYear();
+            people[i]['age'] = year - people[i].yearOfBirth;
+        }
+    }
+
+    const age = people.sort((a, b) => {
+        return b['age'] - a['age'];
+    });
+
+    return age[0];
 
 };
 
