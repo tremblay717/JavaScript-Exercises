1543667179b38e6973549e7fc2f0d3acc96d7b13
christophe <christo_hurley@hotmail.com>
2022-07-09 11:32:46 -0400

    Finished getTheTitles Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let i = 0;
+    let array = []
 
+for (const book in books)  {
+  array.push(books[i].title)
+  i++
+}
+return array;
 };
 
 // Do not edit below this line
